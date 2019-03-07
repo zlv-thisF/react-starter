9470c1464187a42d89fdd8a9c8a97f4a51f18f5b
lvzhe <lvzhe@maxtropy.com>
2019-03-07 17:40:23 +0800

    feat: typescript pure branch : done

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import React from "react";
 
+export const Users = React.lazy(() => import("@/pages/users"));
+
 export const Dashboard = React.lazy(() => import("@/pages/dashboard"));
 
 export const Goods = React.lazy(() => import("@/pages/goods"));
-
-export const Users = React.lazy(() => import("@/pages/users"));
